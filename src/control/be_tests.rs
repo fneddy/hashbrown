6d35f8bb40c1460e4430bb6b75b53c364e461e49
Eddy (Eduard) Stefes <eddy@linux.ibm.com>
2026-05-26 14:02:23 +0200

    fix test

--- src/control/be_tests.rs
+++ src/control/be_tests.rs
@@ -6,16 +6,38 @@ fn group_match_masks_follow_memory_order() {
     let width = Group::WIDTH;
     assert!(width >= 8);
 
-    let filler = Tag::full(4 << 57);
+    let tag0_hash = 0_u64;
+    let tag1_hash = 1_u64 << 57;
+    let tag2_hash = 2_u64 << 57;
+    let tag3_hash = 3_u64 << 57;
+    let filler_hash = 4_u64 << 57;
+
+    let tag0 = Tag::full(tag0_hash);
+    let tag1 = Tag::full(tag1_hash);
+    let tag2 = Tag::full(tag2_hash);
+    let tag3 = Tag::full(tag3_hash);
+    let filler = Tag::full(filler_hash);
+
     let mut tags = vec![filler; width];
     tags[0] = Tag::DELETED;
-    tags[1] = Tag::full(0);
+    tags[1] = tag0;
     tags[2] = Tag::EMPTY;
-    tags[3] = Tag::full(1 << 57);
+    tags[3] = tag1;
     tags[4] = Tag::DELETED;
     tags[5] = Tag::EMPTY;
-    tags[6] = Tag::full(2 << 57);
-    tags[width - 1] = Tag::full(3 << 57);
+    tags[6] = tag2;
+    tags[width - 1] = tag3;
+
+    assert_ne!(tag0, tag1);
+    assert_ne!(tag0, tag2);
+    assert_ne!(tag0, tag3);
+    assert_ne!(tag0, filler);
+    assert_ne!(tag1, tag2);
+    assert_ne!(tag1, tag3);
+    assert_ne!(tag1, filler);
+    assert_ne!(tag2, tag3);
+    assert_ne!(tag2, filler);
+    assert_ne!(tag3, filler);
 
     let group = unsafe { Group::load(tags.as_ptr()) };
 
@@ -25,17 +47,17 @@ fn group_match_masks_follow_memory_order() {
     let empty: Vec<_> = group.match_empty().into_iter().collect();
     assert_eq!(empty, vec![2, 5]);
 
-    let tag0: Vec<_> = group.match_tag(Tag::full(0)).into_iter().collect();
-    assert_eq!(tag0, vec![1]);
+    let tag0_matches: Vec<_> = group.match_tag(tag0).into_iter().collect();
+    assert_eq!(tag0_matches, vec![1]);
 
-    let tag1: Vec<_> = group.match_tag(Tag::full(1 << 57)).into_iter().collect();
-    assert_eq!(tag1, vec![3]);
+    let tag1_matches: Vec<_> = group.match_tag(tag1).into_iter().collect();
+    assert_eq!(tag1_matches, vec![3]);
 
-    let tag2: Vec<_> = group.match_tag(Tag::full(2 << 57)).into_iter().collect();
-    assert_eq!(tag2, vec![6]);
+    let tag2_matches: Vec<_> = group.match_tag(tag2).into_iter().collect();
+    assert_eq!(tag2_matches, vec![6]);
 
-    let tag3: Vec<_> = group.match_tag(Tag::full(3 << 57)).into_iter().collect();
-    assert_eq!(tag3, vec![width - 1]);
+    let tag3_matches: Vec<_> = group.match_tag(tag3).into_iter().collect();
+    assert_eq!(tag3_matches, vec![width - 1]);
 
     let filler_matches: Vec<_> = group.match_tag(filler).into_iter().collect();
     let mut expected_filler = Vec::new();
