badcc44c15b14137c407f0c1382150adb909ae4d
Eddy (Eduard) Stefes <eddy@linux.ibm.com>
2026-05-26 14:29:02 +0200

    check for collision

--- tests/pointer_keys.rs
+++ tests/pointer_keys.rs
@@ -3,18 +3,20 @@
 #![cfg(not(miri))]
 
 use hashbrown::HashMap;
+use std::ptr::NonNull;
+
+fn make_ptr(index: usize) -> *const u8 {
+    NonNull::<u8>::dangling().as_ptr().wrapping_add(index)
+}
 
 #[test]
 fn pointer_keys_match_values_after_large_insert() {
     let len = 1 << 20;
-    let data = vec![0_u64; len];
-    let base = data.as_ptr();
-
     let mut map = HashMap::with_capacity(len);
 
     for index in 0..len {
-        let ptr = base.wrapping_add(index);
-        map.insert(ptr, ptr);
+        let ptr = make_ptr(index);
+        assert_eq!(map.insert(ptr, ptr), None);
     }
 
     assert_eq!(map.len(), len);
@@ -27,24 +29,21 @@ fn pointer_keys_match_values_after_large_insert() {
 #[test]
 fn pointer_keys_match_values_after_churn() {
     let len = 1 << 18;
-    let data = vec![0_u64; len];
-    let base = data.as_ptr();
-
     let mut map = HashMap::with_capacity(len);
 
     for index in 0..len {
-        let ptr = base.wrapping_add(index);
-        map.insert(ptr, ptr);
+        let ptr = make_ptr(index);
+        assert_eq!(map.insert(ptr, ptr), None);
     }
 
     for index in (0..len).step_by(3) {
-        let ptr = base.wrapping_add(index);
+        let ptr = make_ptr(index);
         assert_eq!(map.remove(&ptr), Some(ptr));
     }
 
     for index in (0..len).step_by(3) {
-        let ptr = base.wrapping_add(index);
-        map.insert(ptr, ptr);
+        let ptr = make_ptr(index);
+        assert_eq!(map.insert(ptr, ptr), None);
     }
 
     assert_eq!(map.len(), len);
